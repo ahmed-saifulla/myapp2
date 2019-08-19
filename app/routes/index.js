648e25d1a4a2225a2e60e5608f021e1f3835b961
ahmed-saifulla <ahsaifulla@gmail.com>
2019-08-19 07:54:30 +0530

    blah blah

--- app/routes/index.js
+++ app/routes/index.js
@@ -11,16 +11,23 @@ app.use(bodyParser.urlencoded({extended : true}));
 
 // Handling a GET request to this file (eg: server.js) from browser
 router.get('/' , (req,res) => {                   //Callback function
-    res.send('hello express world!');
+    // res.send('hello express world!');
+    res.sendFile(path.join(__dirname+'/views/home.html'));
 });
 
 router.get('/auth',function(req,res){
     res.sendFile(path.join(__dirname+'/views/auth.html'));
-  });
-
+});
 
 router.post('/auth', (req,res) => {
-    console.log(req.body.username);
+    
+    username = req.body.username;
+    password = req.body.password;
+
+    console.log('\n Login attempt by : \n ------------------------');
+    console.log(`username :${username}`);
+    console.log(`password :${password}`);
+
     res.send();
 });
 
